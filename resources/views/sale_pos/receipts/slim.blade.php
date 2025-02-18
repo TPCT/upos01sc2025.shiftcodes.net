26b81f765d26ac1c0a06c2b0c412c8153b5b1099
tpct <eslamjackie@outlook.com>
2025-02-18 16:05:33 +0200

    adding first screen tables

--- resources/views/sale_pos/receipts/slim.blade.php
+++ resources/views/sale_pos/receipts/slim.blade.php
@@ -18,89 +18,89 @@
 			word-break: break-all;
 		}
 
-		.logo img{
+		.invoice-slim .logo img{
 			margin: auto;
 			height: 80px;
 			object-fit: contain;
 		}
-		.container {
+		.invoice-slim .container {
 			margin: 20px auto;
 			border: 2px solid #000;
 			padding: 5px;
 		}
 
-		.header {
+		.invoice-slim .header {
 			text-align: center;
 			margin-bottom: 20px;
 			line-height: 1.8;
 			border-bottom: 4px solid #000;
 		}
 
-		.header h2 {
+		.invoice-slim .header h2 {
 			margin: 0;
 			font-size: 12px;
 		}
 
-		.header p {
+		.invoice-slim .header p {
 			margin: 5px 0;
 		}
 
-		.details p {
+		.invoice-slim .details p {
 			margin: 5px 0;
 		}
 
-		table {
+		.invoice-slim table {
 			width: 100%;
 			border-collapse: collapse;
 			margin-top: 20px;
 		}
 
-		th,
-		td {
+		.invoice-slim th,
+		.invoice-slim td {
 			border: 1px solid #000;
 			padding: 10px;
 			text-align: center;
 		}
 
-		.summary {
+		.invoice-slim .summary {
 			margin-top: 20px;
 		}
 
-		.summary-table {
+		.invoice-slim .summary-table {
 			margin-right: auto;
 		}
 
-		.footer {
+		.invoice-slim .footer {
 			text-align: center;
 			margin-top: 30px;
 			font-weight: bold;
 		}
 
-		.summary-quantity {
+		.invoice-slim .summary-quantity {
 			display: flex;
 			justify-content: space-between;
 			padding-bottom: 10px;
 
 		}
 
-		.summary-quantity span {
+		.invoice-slim .summary-quantity span {
 			padding: 3px 22px;
 		}
 
-		.summary-head {
+		.invoice-slim .summary-head {
 			display: flex;
 			justify-content: space-around;
 
 		}
 
-		.summary-head p {
+		.invoice-slim .summary-head p {
 			display: flex;
 			align-items: center;
 			gap: 30px;
 			padding: 10px;
 		}
 
-		.info-details {
+		.invoice-slim .info-details {
 			display: flex;
 			align-items: center;
 			gap: 50px;
@@ -131,26 +131,22 @@
 
 	<div style="display: flex; flex-direction: column; margin-right: 20px; margin-left: 80px;">
 		<p>التاريخ: {{$receipt_details->invoice_date}}</p>
-		@if(!empty($receipt_details->total_due))
-			<p>فاتورة مبيعات - أجل</p>
-		@elseif(!empty($receipt_details->total_paid))
-			<p>فاتورة مبيعات - نقدي</p>
-		@else
-			<p>فاتورة مبيعات - عرض سعر</p>
-		@endif
+		<div style="display: flex; justify-content: center">
+			@if(!empty($receipt_details->total_due))
+				<p>فاتورة مبيعات - أجل</p>
+			@elseif(!empty($receipt_details->total_paid))
+				<p>فاتورة مبيعات - نقدي</p>
+			@else
+				<p>فاتورة مبيعات - عرض سعر</p>
+			@endif
+		</div>
 	</div>
 
 	<div class="details">
-		<div class="info-details">
-
-			<p><strong>التاريخ:</strong> {{$receipt_details->transaction_date_object->format("d/m/Y")}} - الساعة: {{$receipt_details->transaction_date_object->format("h a")}}</p>
-			<p><strong>رقم الفاتورة:</strong> {{$receipt_details->invoice_no}}</p>
-		</div>
-		<div class="info-details">
-			<p><strong>اسم العميل:</strong> {{$receipt_details->customer_name}}</p>
-			<p><strong>الهاتف:</strong> {{$receipt_details->customer_mobile}}</p>
-
-		</div>
+		<p><strong>التاريخ:</strong> {{$receipt_details->transaction_date_object->format("d/m/Y")}} - الساعة: {{$receipt_details->transaction_date_object->format("h a")}}</p>
+		<p><strong>رقم الفاتورة:</strong> {{$receipt_details->invoice_no}}</p>
+		<p><strong>اسم العميل:</strong> {{$receipt_details->customer_name}}</p>
+		<p><strong>الهاتف:</strong> {{$receipt_details->customer_mobile}}</p>
 		@if ($receipt_details->customer_city)
 			<p><strong>مدينة:</strong> {{$receipt_details->customer_city}}</p>
 		@endif
@@ -186,21 +182,17 @@
 				@endphp
 				<tr>
 					<td>{{$line['name']}}</td>
-					<td>{{$line['unit_price_inc_tax'] . " " . $receipt_details->currency['symbol']}}</td>
+					<td>{{$line['unit_price_inc_tax']}}</td>
 					<td>{{(float)$line['quantity']}}</td>
-					<td>{{number_format($discount, 2) . " " . $receipt_details->currency['symbol']}}</td>
-					<td>{{number_format($total, 2) . " " . $receipt_details->currency['symbol']}}</td>
+					<td>{{number_format($discount, 2)}}</td>
+					<td>{{number_format($total, 2)}}</td>
 				</tr>
 			@endforeach
 		</tbody>
 	</table>
 
 	<div class="summary">
-		<div class="summary-head" style="display: flex; flex-direction: column">
-			<p><strong>المجموع:</strong> <span>{{number_format($total_sum, 2) . " " . $receipt_details->currency['symbol']}}</span> </p>
-			<p><strong>الخصم:</strong> <span>{{number_format($total_discount, 2) . " " . $receipt_details->currency['symbol']}}</span> </p>
-			<p><strong>الإجمالي:</strong> <span>{{number_format($total_sum - $total_discount, 2) . " " . $receipt_details->currency['symbol']}}</span> </p>
-		</div>
+
 		<div class="summary-quantity" style="display: flex; flex-direction: column">
 			<p><strong>عدد الأصناف:</strong> <span>{{$total_products}}</span> </p>
 			<p><strong>إجمالى الكمية:</strong> <span>{{$total_quantity}}</span> </p>
@@ -209,6 +201,11 @@
 		<div style="height: 1px; border: 1px solid black; margin-top: 10px"></div>
 
 		<table class="summary-table">
+			<tr>
+				<td>المجموع:</td> <td>{{number_format($total_sum, 2) . " " . $receipt_details->currency['symbol']}}</td>
+				<td>الخصم:</td> <td>{{number_format($total_discount, 2) . " " . $receipt_details->currency['symbol']}}</td>
+				<td>الإجمالي:</td> <td>{{number_format($total_sum - $total_discount, 2) . " " . $receipt_details->currency['symbol']}}</td>
+			</tr>
 			<tr>
 				<td><strong>الإجمالي:</strong></td>
 				<td>{{number_format($total_sum - $total_discount, 2) . " " . $receipt_details->currency['symbol']}}</td>
