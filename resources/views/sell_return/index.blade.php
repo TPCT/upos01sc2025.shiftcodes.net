ee6e4a6e13c65ad8cdf36d6d9f040d9cb7438c5e
tpct <eslamjackie@outlook.com>
2025-02-24 09:06:07 +0200

    adding first screen tables

--- resources/views/sell_return/index.blade.php
+++ resources/views/sell_return/index.blade.php
@@ -42,6 +42,22 @@
         @endcan
     @endcomponent
     @component('components.widget', ['class' => 'box-primary', 'title' => __('lang_v1.sell_return')])
+            @can('direct_sell.access')
+                @slot('tool')
+                    <div class="box-tools">
+                        <a class="tw-dw-btn tw-bg-gradient-to-r tw-from-indigo-600 tw-to-blue-500 tw-font-bold tw-text-white tw-border-none tw-rounded-full pull-right"
+                           href="{{ action([\App\Http\Controllers\SellReturnController::class, 'create']) }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                 class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 5l0 14" />
+                                <path d="M5 12l14 0" />
+                            </svg> @lang('messages.add')
+                        </a>
+                    </div>
+                @endslot
+            @endcan
         @include('sell_return.partials.sell_return_list')
     @endcomponent
     <div class="modal fade payment_modal" tabindex="-1" role="dialog" 
